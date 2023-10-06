50ea08c0ee1aa7612629a4babb54148492bac1dd
Danm1928 <114197770+Danm1928@users.noreply.github.com>
2023-10-06 16:41:01 +1300

    Add files via upload

--- bleu.py
+++ bleu.py
@@ -4,8 +4,6 @@ import statistics
 
 reference = open(r"C:\Users\khore\OneDrive\Desktop\DATA309\bleu\sampling.txt", "r", encoding="utf8").read().splitlines()
 
-# print(reference)
-
 # Specify the folder path where your text files are located
 folder_path = r'C:\Users\khore\OneDrive\Desktop\DATA309\bleu\samples'
 
@@ -23,12 +21,14 @@ for filename in os.listdir(folder_path):
         with open(file_path, 'r') as file:
             candidates.append(file.read().splitlines())
 
-
+# Split candidate samples into a single list of each line
 candidate = []
 for x in candidates:
     for y in x:
         candidate.append(y)
-print(candidate)
+
+# Calculate score for every line and take the average. It has been done this way because of the way that bleu score works, but also because
+# there is no way to tell where a song ends and another starts in the original data
 scores = []
 for x in candidate:
     scores.append(sentence_bleu(reference, x))
